fc40518d32a92e21b43c3e322aff69f3eb6abaad
Javier Hernández Gil <javibilboweb@gmail.com>
2015-09-08 18:27:45 +0200

    Configuration base class

---
+++ src/Config/Configuration.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Jhg\PhpHooker\Config;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * Class Configuration
+ * 
+ * @package Jhg\PhpHooker\Config
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('phphooker');
+
+        $rootNode
+            ->children()
+                ->booleanNode('debug')
+                    ->defaultFalse()
+                ->end()
+
+                ->arrayNode('global')
+                    ->treatNullLike([])
+                    ->prototype('variable')->end()
+                ->end()
+
+                ->arrayNode('checkers')
+                    ->treatNullLike([])
+                    ->prototype('scalar')->end()
+                ->end()
+            ->end() //children
+        ;
+
+        return $treeBuilder;
+    }
+}
---
+++ src/Tests/Integration/Config/ConfigurationTest.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Tests\Integration\Config;
+
+use Jhg\PhpHooker\Config\Configuration;
+use Symfony\Component\Config\Definition\Processor;
+
+/**
+ * Class ConfigurationTest
+ *
+ * @package Tests\Integration\Config
+ */
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @return array
+     */
+    public function configurationProvider()
+    {
+        return [
+            // default configuration
+            [
+                [],
+                [
+                    'debug' => false,
+                    'global' => [],
+                    'checkers' => [],
+                ],
+            ],
+
+            // debug true configuration
+            [
+                [
+                    'debug' => true
+                ],
+                [
+                    'debug' => true,
+                    'global' => [],
+                    'checkers' => [],
+                ],
+            ],
+
+            // global configuration
+            [
+                [
+                    'global' => [
+                        'boolean' => true,
+                        'string' => 'test',
+                        'integer' => 1923,
+                        'array' => [
+                            'test' => 'ok',
+                        ]
+                    ]
+                ],
+                [
+                    'debug' => false,
+                    'global' => [
+                        'boolean' => true,
+                        'string' => 'test',
+                        'integer' => 1923,
+                        'array' => [
+                            'test' => 'ok',
+                        ]
+                    ],
+                    'checkers' => [],
+                ],
+            ],
+
+            // custom checkers configuration
+            [
+                [
+                    'checkers' => [
+                        'test' => 'Class\\Complete\\Path',
+                        'test2' => 'Class\\Complete\\Path',
+                    ]
+                ],
+                [
+                    'debug' => false,
+                    'global' => [],
+                    'checkers' => [
+                        'test' => 'Class\\Complete\\Path',
+                        'test2' => 'Class\\Complete\\Path',
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @param array $phphookerConfig
+     * @param array $expectedConfig
+     *
+     * @dataProvider configurationProvider
+     */
+    public function testProcessConfiguration(array $phphookerConfig, array $expectedConfig)
+    {
+        $configuration = new Configuration();
+        $processor = new Processor();
+
+        $returnedConfig = $processor->processConfiguration($configuration, ['phphooker' => $phphookerConfig]);
+
+        $this->assertEquals($expectedConfig, $returnedConfig);
+    }
+}
